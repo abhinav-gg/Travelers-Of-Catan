3f12fed8b9548ed111c7f3d19453658c9c68d41f
Abhinav <agupta.cam7@gmail.com>
2023-04-27 10:08:39 +0100

    MVP

--- test/Ui.py
+++ test/Ui.py
@@ -1,3 +1,4 @@
+from Game import Game
 from abc import ABC, abstractmethod
 
 class Ui(ABC):
@@ -15,7 +16,15 @@ class Gui(Ui):
 
 class Terminal(Ui):
     def __init__(self):
-        pass
+        self._game = Game()
 
     def run(self):
-        pass
+        while not self._game.winner:
+            print(self._game)
+            row = int(input("Which row? "))
+            col = int(input("Which column? "))
+            self._game.play(row,col)
+
+        print(self._game)
+        w = self._game.winner
+        print(f"The winner was {w}")
