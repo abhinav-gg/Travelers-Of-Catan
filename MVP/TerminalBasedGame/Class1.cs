418b508390441037afdf8d14d352edaa7cedf4c2
Abhinav <agupta.cam7@gmail.com>
2023-06-27 10:30:08 +0100

    Board Complete. Need Game Mechanics

--- MVP/TerminalBasedGame/Class1.cs
+++ MVP/TerminalBasedGame/Class1.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace App
 {
-    class Class1
+    public static class MainFunc
     {
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Game! Key [ENTER] to begin...");
             Console.ReadLine();
+            GamePlayLoop game = new GamePlayLoop(2);
+            game.startGame();
         }
+
+
+        public static Vector3 GetNextPosition()
+        {
+            string playerPos =  Console.ReadLine();
+            string[] pos = playerPos.Split(',');
+            return new Vector3(int.Parse(pos[0]), int.Parse(pos[1]), int.Parse(pos[2]));
+        }
+
+
     }
 
 
@@ -57,39 +71,52 @@ namespace App
 namespace TerminalBasedGame
 {
 
-    class Resource
-    {
-        private int i;
-        private string[] resources = { "Wood", "Brick", "Wheat", "Sheep", "Ore" };
 
-        public Resource(int i)
-        {
-            this.i = i;
-        }
-
-        public string getResource()
-        {
-            return resources[i];
-        }
-
-    }
 
     class Node
     {
-        public List<Node> neighbours;
+        public Vector3 position;
         private BuildingStatus status;
 
         public Node(BuildingStatus status)
         {
             this.status = status;
         }
+
+
+        public IEnumerable<Vector3> GetNeighbours()
+        {
+
+            // determine parity of position
+            int sum = (int)(position.X + position.Y + position.Z);
+            if (sum % 2 == 0)
+            {
+                // even
+                
+                
+                yield return position + new Vector3(1, 0, 0);
+                yield return position + new Vector3(0, -1, 0);
+                yield return position + new Vector3(0, 0, 1);
+
+            }
+            else
+            {
+                yield return position + new Vector3(-1, 0, 0);
+                yield return position + new Vector3(0, 1, 0);
+                yield return position + new Vector3(0, 0, -1);
+
+            }
+            yield return new Vector3();
+        }
+
+
     }
 
     class HexagonUnit
     {
+        public Vector3 position;
         private Resource m_resource;
         private Node[] nodes = new Node[6];
-
 
         public HexagonUnit(Resource R)
         {
@@ -99,19 +126,22 @@ namespace TerminalBasedGame
             for (int i = 0; i < 6; i++)
             {
                 nodes[i] = new Node(new BuildingStatus(0));
-                nodes[i].neighbours.Add(nodes[(i + 1) % 6]);
-                nodes[i].neighbours.Add(nodes[(i + 5) % 6]);
             }
 
         }
 
-        
-
-        public string getResource()
+        public IEnumerable<Vector3> GetNeighbours()
         {
-            return m_resource.getResource();
-        }
+            yield return position + new Vector3(1, 0, 0);
+            yield return position + new Vector3(0, 1, 0);
+            yield return position + new Vector3(0, 0, 1);
+            yield return position + new Vector3(-1, 0, 0);
+            yield return position + new Vector3(0, -1, 0);
+            yield return position + new Vector3(0, 0, -1);
 
+        }  
+
+       
     }
 
     class Board
@@ -126,6 +156,21 @@ namespace TerminalBasedGame
                 board[i] = new HexagonUnit(new Resource(0));
             }
         }
+
+        public HexagonUnit GetUnitAtPosition(Vector3 pos)
+        {
+            foreach (HexagonUnit unit in board)
+            {
+                if (unit.position == pos)
+                {
+                    return unit;
+                }
+            }
+            return null;
+        }
+
+
+
     }
 
     class BuildingStatus
@@ -148,6 +193,31 @@ namespace TerminalBasedGame
         public ConnectionStatus(int i)
         {
             this.i = i;
+        }
+
+    }
+
+
+
+    class Resource
+    {
+        private int i;
+        private string[] resources = { "Wood", "Brick", "Wheat", "Sheep", "Ore" };
+
+        public Resource(int i)
+        {
+            this.i = i;
+        }
+
+        public string getResourceString()
+        {
+            return resources[i];
+        }
+
+        public Resource GenerateRandomResource() 
+        { 
+            Random rnd = new Random();
+            return new Resource(rnd.Next(0, resources.Length));
         }
 
     }
